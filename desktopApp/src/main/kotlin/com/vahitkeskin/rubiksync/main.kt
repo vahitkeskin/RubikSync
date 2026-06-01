4e489da106ee6d8a791d9049a4b6c439f41c9c90
Vahit Keskin <36104238+vahitkeskin@users.noreply.github.com>
2026-06-01 18:41:01 +0300

    - Add app icon

--- desktopApp/src/main/kotlin/com/vahitkeskin/rubiksync/main.kt
+++ desktopApp/src/main/kotlin/com/vahitkeskin/rubiksync/main.kt
@@ -2,11 +2,13 @@ package com.vahitkeskin.rubiksync
 
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import androidx.compose.ui.res.painterResource
 
 fun main() = application {
     Window(
         onCloseRequest = ::exitApplication,
         title = "RubikSync",
+        icon = painterResource("app_icon.png")
     ) {
         App()
     }

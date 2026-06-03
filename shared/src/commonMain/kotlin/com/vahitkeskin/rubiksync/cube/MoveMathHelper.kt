2339c352a8d62a7649b4782168fc205f8708894b
Vahit Keskin <36104238+vahitkeskin@users.noreply.github.com>
2026-06-03 23:44:13 +0300

    Improve rotation matrix rendering using unicode box-drawing characters

--- shared/src/commonMain/kotlin/com/vahitkeskin/rubiksync/cube/MoveMathHelper.kt
+++ shared/src/commonMain/kotlin/com/vahitkeskin/rubiksync/cube/MoveMathHelper.kt
@@ -1,5 +1,22 @@
 package com.vahitkeskin.rubiksync.cube
 
+private fun formatMatrix(
+    m11: Int, m12: Int, m13: Int,
+    m21: Int, m22: Int, m23: Int,
+    m31: Int, m32: Int, m33: Int
+): String {
+    fun cell(v: Int): String = if (v < 0) " $v" else "  $v"
+    
+    val r1 = "│ ${cell(m11)} ${cell(m12)} ${cell(m13)} │"
+    val r2 = "│ ${cell(m21)} ${cell(m22)} ${cell(m23)} │"
+    val r3 = "│ ${cell(m31)} ${cell(m32)} ${cell(m33)} │"
+    
+    val top = "┌" + " ".repeat(13) + "┐"
+    val bottom = "└" + " ".repeat(13) + "┘"
+    
+    return "$top\n$r1\n$r2\n$r3\n$bottom"
+}
+
 fun getMoveMathDetails(move: MoveType): String {
     val axisStr = when {
         move.axis.x > 0.5f -> "X = (1, 0, 0)"
@@ -18,31 +35,23 @@ fun getMoveMathDetails(move: MoveType): String {
     val angleDeg = if (move.angleSign > 0) "90°" else "-90°"
     val angleRadSymbol = if (move.angleSign > 0) "π / 2" else "-π / 2"
     
+    val s = move.angleSign.toInt()
     val matrixStr = when {
-        move.axis.x > 0.5f -> {
-            val s = move.angleSign.toInt()
-            """
-            |  1     0     0  |
-            |  0     0    ${if (s > 0) "-1" else " 1"}  |
-            |  0    ${if (s > 0) " 1" else "-1"}     0  |
-            """.trimIndent()
-        }
-        move.axis.y > 0.5f -> {
-            val s = move.angleSign.toInt()
-            """
-            |  0     0    ${if (s > 0) " 1" else "-1"}  |
-            |  0     1     0  |
-            | ${if (s > 0) "-1" else " 1"}     0     0  |
-            """.trimIndent()
-        }
-        move.axis.z > 0.5f -> {
-            val s = move.angleSign.toInt()
-            """
-            |  0    ${if (s > 0) "-1" else " 1"}     0  |
-            | ${if (s > 0) " 1" else "-1"}     0     0  |
-            |  0     0     1  |
-            """.trimIndent()
-        }
+        move.axis.x > 0.5f -> formatMatrix(
+            1, 0, 0,
+            0, 0, -s,
+            0, s, 0
+        )
+        move.axis.y > 0.5f -> formatMatrix(
+            0, 0, s,
+            0, 1, 0,
+            -s, 0, 0
+        )
+        move.axis.z > 0.5f -> formatMatrix(
+            0, -s, 0,
+            s, 0, 0,
+            0, 0, 1
+        )
         else -> "N/A"
     }
 
@@ -67,3 +76,4 @@ fun getMoveMathDetails(move: MoveType): String {
         f' = R • f
     """.trimIndent()
 }
+

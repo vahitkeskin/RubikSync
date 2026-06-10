4702155d39e73a4d211cc40f97e6e7c604f7baa3
Vahit Keskin <36104238+vahitkeskin@users.noreply.github.com>
2026-06-10 18:19:30 +0300

    feat: user custom preview configuration for RubikToolbar

--- shared/src/commonMain/kotlin/com/vahitkeskin/rubiksync/ui/components/RubikToolbar.kt
+++ shared/src/commonMain/kotlin/com/vahitkeskin/rubiksync/ui/components/RubikToolbar.kt
@@ -3,7 +3,16 @@ package com.vahitkeskin.rubiksync.ui.components
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
@@ -13,16 +22,14 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.vahitkeskin.rubiksync.ui.icons.ArrowBackIcon
-import com.vahitkeskin.rubiksync.ui.icons.CloseIcon
-import com.vahitkeskin.rubiksync.ui.state.RubikTheme
-import androidx.compose.ui.tooling.preview.Preview
-import com.vahitkeskin.rubiksync.ui.state.PreviewRubikTheme
-import androidx.compose.ui.graphics.Color
 import com.vahitkeskin.rubiksync.ui.icons.GalleryIcon
+import com.vahitkeskin.rubiksync.ui.state.PreviewRubikTheme
+import com.vahitkeskin.rubiksync.ui.state.RubikTheme
 
 /**
  * A highly customizable, reusable top toolbar component for RubikSync screens.

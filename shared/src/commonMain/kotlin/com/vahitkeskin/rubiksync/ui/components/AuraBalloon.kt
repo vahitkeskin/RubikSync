a17b8ac4e0e6826bc2931b4d41daaa3218d7ae56
Vahit Keskin <36104238+vahitkeskin@users.noreply.github.com>
2026-06-12 02:10:52 +0300

    style: auto-format AuraBalloon.kt

--- shared/src/commonMain/kotlin/com/vahitkeskin/rubiksync/ui/components/AuraBalloon.kt
+++ shared/src/commonMain/kotlin/com/vahitkeskin/rubiksync/ui/components/AuraBalloon.kt
@@ -216,7 +216,11 @@ class BalloonShape(
     private val isBelow: Boolean,
     private val cornerRadius: Float
 ) : Shape {
-    override fun createOutline(size: Size, layoutDirection: LayoutDirection, density: Density): Outline {
+    override fun createOutline(
+        size: Size,
+        layoutDirection: LayoutDirection,
+        density: Density
+    ): Outline {
         val path = Path()
         val w = size.width
         val h = size.height
@@ -225,49 +229,49 @@ class BalloonShape(
         if (isBelow) {
             val top = arrowHeight
             val bottom = h
-            
+
             path.moveTo(cr, top)
             path.lineTo(arrowX - arrowWidth / 2f, top)
             path.lineTo(arrowX, 0f)
             path.lineTo(arrowX + arrowWidth / 2f, top)
-            
+
             path.lineTo(w - cr, top)
             path.arcTo(Rect(w - cr * 2, top, w, top + cr * 2), -90f, 90f, false)
-            
+
             path.lineTo(w, bottom - cr)
             path.arcTo(Rect(w - cr * 2, bottom - cr * 2, w, bottom), 0f, 90f, false)
-            
+
             path.lineTo(cr, bottom)
             path.arcTo(Rect(0f, bottom - cr * 2, cr * 2, bottom), 90f, 90f, false)
-            
+
             path.lineTo(0f, top + cr)
             path.arcTo(Rect(0f, top, cr * 2, top + cr * 2), 180f, 90f, false)
-            
+
             path.close()
         } else {
             val top = 0f
             val bottom = h - arrowHeight
-            
+
             path.moveTo(cr, top)
             path.lineTo(w - cr, top)
             path.arcTo(Rect(w - cr * 2, top, w, top + cr * 2), -90f, 90f, false)
-            
+
             path.lineTo(w, bottom - cr)
             path.arcTo(Rect(w - cr * 2, bottom - cr * 2, w, bottom), 0f, 90f, false)
-            
+
             path.lineTo(arrowX + arrowWidth / 2f, bottom)
             path.lineTo(arrowX, h)
             path.lineTo(arrowX - arrowWidth / 2f, bottom)
-            
+
             path.lineTo(cr, bottom)
             path.arcTo(Rect(0f, bottom - cr * 2, cr * 2, bottom), 90f, 90f, false)
-            
+
             path.lineTo(0f, top + cr)
             path.arcTo(Rect(0f, top, cr * 2, top + cr * 2), 180f, 90f, false)
-            
+
             path.close()
         }
-        
+
         return Outline.Generic(path)
     }
 }
